cc3b0283af3b71fd69fe1bfef56134907100ad33
aizaburkhan <aizada.wedf@gmail.com>
2023-12-21 19:17:30 +0000

    Added HW2

--- Homework2/instance.tf
+++ Homework2/instance.tf
@@ -1,27 +1,27 @@
-data "aws_ami" "ubuntu" {
+data "aws_ami" "amazon" {
   most_recent = true
-   #ami id = ami-07b36ea9852e986ad
-   #should pick the lates one with the date: 2023/10/26 00:01 GMT-4
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
+  owners = ["amazon"] 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
-  owners = ["099720109477"] # Canonical
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  } 
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = data.aws_ami.amazon.id
   instance_type = "t2.micro"
   availability_zone = "us-east-2a"
   vpc_security_group_ids = [aws_security_group.homework_sg.id] # add up to 5 sgs possible
   key_name = aws_key_pair.homework_key.key_name
-  user_data = file("apache2.sh")
+  user_data = file("apache.sh")
   tags = local.common_tags
   }
 
