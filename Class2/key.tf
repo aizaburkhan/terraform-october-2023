938cd73e174f0bc4b6fa989a1ec15a4ece6890e2
aizaburkhan <153569530+aizaburkhan@users.noreply.github.com>
2024-03-25 22:02:32 -0400

    Update key.tf
    commented out the key part/ uncomment later

--- Class2/key.tf
+++ Class2/key.tf
@@ -1,8 +1,8 @@
-resource "aws_key_pair" "parent" {
-  key_name   = "my-key"
-  public_key = file("~/.ssh/id_rsa.pub")
-  tags = local.common_tags
-}
+#resource "aws_key_pair" "parent" {
+#  key_name   = "my-key"
+# public_key = file("~/.ssh/id_rsa.pub")
+# tags = local.common_tags
+#}
 
 # output "key_type_info" {
 #   value=aws_key_pair.parent.key_type
